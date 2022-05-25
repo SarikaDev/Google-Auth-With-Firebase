ad0711dd75da4f8d5889e047061fe75f4ce3693f
Sarika.Sairam <96573888+SarikaDev@users.noreply.github.com>
2022-05-25 10:17:08 +0530

    made changes in app App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Signup, Login, Dashboard, ForgotPassword, UpdateProfile } from './Components/index2';
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-// import PrivateRoutes from './PrivateRoutes/PrivateRoutes';
 
 function App() {
   return (
@@ -11,10 +10,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Signup />} />
           <Route path='/login' element={<Login />} />
-          {/* <Route element={<PrivateRoutes/>}> */}
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/updateProfile' element={<UpdateProfile />} />
-          {/* </Route> */}
             <Route path='/forgotPassword' element={<ForgotPassword />} />
         </Routes>
       </AuthProvider>
