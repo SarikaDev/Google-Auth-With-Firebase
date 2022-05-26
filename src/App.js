15b1bfe1f334128885d85a11690459af3ec09a03
Sarika.Sairam <96573888+SarikaDev@users.noreply.github.com>
2022-05-26 17:22:28 +0530

    changes in PrivateRoutes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Signup, Login, Dashboard, ForgotPassword, UpdateProfile } from './Components/index2';
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoutes from './PrivateRoutes/PrivateRoutes';
 
 function App() {
   return (
@@ -10,9 +11,12 @@ function App() {
         <Routes>
           <Route path='/' element={<Signup />} />
           <Route path='/login' element={<Login />} />
+          <Route  element={<PrivateRoutes/>} >
+
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/updateProfile' element={<UpdateProfile />} />
             <Route path='/forgotPassword' element={<ForgotPassword />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </>
